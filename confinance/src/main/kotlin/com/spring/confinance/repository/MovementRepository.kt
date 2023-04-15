5e0ce84c2502d2cbd0bcdcbfbd75105315f7537f
Thamyrescordeiro <101646936+Thamyrescordeiro@users.noreply.github.com>
2023-04-15 10:35:25 -0300

    Delete confinance directory

--- confinance/src/main/kotlin/com/spring/confinance/repository/MovementRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.spring.confinance.repository
-
-import com.spring.confinance.model.Movement
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MovementRepository : JpaRepository<Movement, Long>
-
--- confinance/src/main/kotlin/com/spring/confinance/repository/ObjectiveRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.spring.confinance.repository
-
-import com.spring.confinance.model.Objective
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ObjectiveRepository : JpaRepository<Objective,Long>
