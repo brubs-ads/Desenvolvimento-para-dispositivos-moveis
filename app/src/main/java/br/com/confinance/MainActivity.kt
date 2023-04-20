6f140a0112d23c17df7c3def414f8eab5e9db014
Selton A. De Melo <101484450+SeltonAlves@users.noreply.github.com>
2023-04-20 01:16:44 -0300

    tela de abertura 90%
    Tela de abertura quase concluída, falta apenas o ícone do app para fazemos a ligação para outra activity.

--- app/src/main/java/br/com/confinance/MainActivity.kt
+++ app/src/main/java/br/com/confinance/MainActivity.kt
@@ -2,10 +2,39 @@ package br.com.confinance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import br.com.confinance.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        setOnClick()
+
     }
+    override fun onClick(view: View) {
+
+        when(view.id){
+            R.id.button_create_account ->{
+                Toast.makeText(this,"TESTEEEE", Toast.LENGTH_SHORT).show()
+            }
+            R.id.text_login ->{
+                Toast.makeText(this,"ESSE AQUI", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun setOnClick() {
+        binding.buttonCreateAccount.setOnClickListener(this)
+        binding.textLogin.setOnClickListener(this)
+    }
+
+
 }
