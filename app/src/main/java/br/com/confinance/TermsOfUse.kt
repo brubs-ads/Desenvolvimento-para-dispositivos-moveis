5ae2a892526792412e375605ff25f9f5ba73f8b5
joaovicttor19 <111836155+joaovicttor19@users.noreply.github.com>
2023-04-21 20:31:59 -0300

    Merge branch 'entrega-07-06-2023' of https://github.com/FiveTech22/Desenvolvimento-para-dispositivos-moveis into entrega-07-06-2023

---
+++ app/src/main/java/br/com/confinance/TermsOfUse.kt
@@ -0,0 +1,11 @@
+package br.com.confinance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsOfUse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_of_use)
+    }
+}
---
+++ app/src/main/java/br/com/confinance/viewmodel/CreateAccountViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.confinance.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class CreateAccountViewModel(application: Application) : AndroidViewModel(application) {
+}
