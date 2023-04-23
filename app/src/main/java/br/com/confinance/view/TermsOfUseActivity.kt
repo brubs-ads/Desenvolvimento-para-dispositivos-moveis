8030baa533ace8990f6c34e59143f2f0f6a8da1a
selton <selton.alvess1@gmail.com>
2023-04-23 14:18:44 -0300

    organização das telas

--- app/src/main/java/br/com/confinance/view/TermsOfUseActivity.kt
+++ app/src/main/java/br/com/confinance/view/TermsOfUseActivity.kt
@@ -1,9 +1,10 @@
-package br.com.confinance
+package br.com.confinance.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.confinance.R
 
-class TermsOfUse : AppCompatActivity() {
+class TermsOfUseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms_of_use)
---
+++ app/src/main/java/br/com/confinance/viewmodel/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.confinance.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+}
