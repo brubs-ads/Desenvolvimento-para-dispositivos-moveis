51d076cefc0e5f55bb52bd4238961c520d4d7440
Selton A. De Melo <selton.alvess1@gmail.com>
2023-04-25 16:08:37 -0300

    tela de login

--- app/src/main/java/com/mycompany/confinance/view/LoginActivity.kt
+++ app/src/main/java/com/mycompany/confinance/view/LoginActivity.kt
@@ -2,9 +2,11 @@ package com.mycompany.confinance.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mycompany.confinance.R
 import com.mycompany.confinance.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -14,5 +16,27 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        handleClick()
+
     }
+
+    private fun handleClick() {
+        binding.imageArrowBackLogin.setOnClickListener(this)
+        binding.buttonLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.image_arrow_back_login ->{
+
+            }
+
+            R.id.button_login ->{
+
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/mycompany/confinance/view/PrivacyPolicesActivity.kt
+++ app/src/main/java/com/mycompany/confinance/view/PrivacyPolicesActivity.kt
@@ -1,17 +1,22 @@
 package com.mycompany.confinance.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mycompany.confinance.R
 import com.mycompany.confinance.databinding.ActivityPrivacyPolicesBinding
 
 class PrivacyPolicesActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityPrivacyPolicesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPrivacyPolicesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.imageArrowBackPrivacyPolices.setOnClickListener {
+            startActivity(Intent(this,CreateAccountActivity::class.java))
+            finish()
+        }
     }
 }
