8ea5c8e145215c5ecce584b03b9cf7af6b4a85e5
Lucas Fernando <107879275+fernandoppm@users.noreply.github.com>
2023-05-07 00:29:47 -0300

    Criação da Activity Salary e Investmentt

---
+++ app/src/main/java/com/mycompany/confinance/view/InvestmentActivity.kt
@@ -0,0 +1,12 @@
+package com.mycompany.confinance.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mycompany.confinance.R
+
+class InvestmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_investment)
+    }
+}
