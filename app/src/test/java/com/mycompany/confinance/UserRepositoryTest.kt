6034a3a2438c09ab6d7914f3e8a715c864812135
Bruna Camilly <110068589+brubs-ads@users.noreply.github.com>
2023-09-30 12:31:04 -0300

    Update UserRepositoryTest.kt

--- app/src/test/java/com/mycompany/confinance/UserRepositoryTest.kt
+++ app/src/test/java/com/mycompany/confinance/UserRepositoryTest.kt
@@ -50,7 +50,8 @@ class UserRepositoryTest {
         val response = Response.success(responseModel)
 
         // Chame a função de ‘login’ e capture o Callback passado para call.enqueue
-        userRepository.login("brunacamilly.ads@gmail.com", "12345678", apiListener)
+        userRepository.login("brunacamilly.ads@gmail.com", "12345678" +
+                "", apiListener)
         Mockito.verify(call).enqueue(callbackCaptor.capture())
 
         // Chame manualmente o método onResponse no Callback capturado
