2717f81a2e24cfcd8bfca8df7cbe92c839efc0a9
Selton A. De Melo <selton.alvess1@gmail.com>
2023-10-11 17:02:31 -0300

    implementação da tela inicial cerca de 20% concluída.

--- app/src/main/java/com/mycompany/confinance/view/activity/MainActivity.kt
+++ app/src/main/java/com/mycompany/confinance/view/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mycompany.confinance.view.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.TextAppearanceSpan
@@ -11,8 +12,12 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.mycompany.confinance.R
 import com.mycompany.confinance.databinding.ActivityMainBinding
+import com.mycompany.confinance.view.activity.user.ForgotPasswordActivity
+import com.mycompany.confinance.view.activity.user.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,119 +39,119 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_objective, R.id.nav_renevue
+                R.id.nav_home
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
         styleFontMenu(navView)
+
+        navView.setNavigationItemSelectedListener {menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_renevue -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
-    private fun styleFontMenu(navView: NavigationView) {
-        val myMenu = navView.menu
-        val planItem = myMenu.findItem(R.id.plan)
-        val sPlan = SpannableString(planItem.title)
-        sPlan.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationItemAppearance),
-            0,
-            sPlan.length,
-            0
-        )
-        planItem.title = sPlan
+private fun styleFontMenu(navView: NavigationView) {
+    val myMenu = navView.menu
+    val planItem = myMenu.findItem(R.id.plan)
+    val sPlan = SpannableString(planItem.title)
+    sPlan.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationItemAppearance),
+        0,
+        sPlan.length,
+        0
+    )
+    planItem.title = sPlan
 
-        val analyzeItem = myMenu.findItem(R.id.analyze)
-        val sAnalyze = SpannableString(analyzeItem.title)
-        sAnalyze.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationItemAppearance),
-            0,
-            sAnalyze.length,
-            0
-        )
-        analyzeItem.title = sAnalyze
+    val analyzeItem = myMenu.findItem(R.id.analyze)
+    val sAnalyze = SpannableString(analyzeItem.title)
+    sAnalyze.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationItemAppearance),
+        0,
+        sAnalyze.length,
+        0
+    )
+    analyzeItem.title = sAnalyze
 
 
-        val homeItem = myMenu.findItem(R.id.nav_home)
-        val sHome = SpannableString(homeItem.title)
-        sHome.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sHome.length,
-            0
-        )
-        homeItem.title = sHome
+    val homeItem = myMenu.findItem(R.id.nav_home)
+    val sHome = SpannableString(homeItem.title)
+    sHome.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
+        0,
+        sHome.length,
+        0
+    )
+    homeItem.title = sHome
 
-        val objectiveItem = myMenu.findItem(R.id.nav_objective)
-        val sObjective = SpannableString(objectiveItem.title)
-        sObjective.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sObjective.length,
-            0
-        )
-        objectiveItem.title = sObjective
 
-        val revenueItem = myMenu.findItem(R.id.nav_renevue)
-        val sRevenue = SpannableString(revenueItem.title)
-        sRevenue.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sRevenue.length,
-            0
-        )
-        revenueItem.title = sRevenue
+    val revenueItem = myMenu.findItem(R.id.nav_renevue)
+    val sRevenue = SpannableString(revenueItem.title)
+    sRevenue.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
+        0,
+        sRevenue.length,
+        0
+    )
+    revenueItem.title = sRevenue
 
-        val expenseItem = myMenu.findItem(R.id.nav_expense)
-        val sExpense = SpannableString(expenseItem.title)
-        sExpense.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sExpense.length,
-            0
-        )
-        expenseItem.title = sExpense
+    val expenseItem = myMenu.findItem(R.id.nav_expense)
+    val sExpense = SpannableString(expenseItem.title)
+    sExpense.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
+        0,
+        sExpense.length,
+        0
+    )
+    expenseItem.title = sExpense
 
-        val graphicItem = myMenu.findItem(R.id.nav_graphic)
-        val sgraphic = SpannableString(graphicItem.title)
-        sgraphic.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sgraphic.length,
-            0
-        )
-        graphicItem.title = sgraphic
+    val graphicItem = myMenu.findItem(R.id.nav_graphic)
+    val sgraphic = SpannableString(graphicItem.title)
+    sgraphic.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
+        0,
+        sgraphic.length,
+        0
+    )
+    graphicItem.title = sgraphic
 
-        val aboutItem = myMenu.findItem(R.id.nav_about_us)
-        val sAbout = SpannableString(aboutItem.title)
-        sAbout.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sAbout.length,
-            0
-        )
-        aboutItem.title = sAbout
+    val aboutItem = myMenu.findItem(R.id.nav_about_us)
+    val sAbout = SpannableString(aboutItem.title)
+    sAbout.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
+        0,
+        sAbout.length,
+        0
+    )
+    aboutItem.title = sAbout
 
-        val termsItem = myMenu.findItem(R.id.nav_terms_of_use)
-        val sTerms = SpannableString(termsItem.title)
-        sTerms.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sTerms.length,
-            0
-        )
-        termsItem.title = sTerms
+    val termsItem = myMenu.findItem(R.id.nav_terms_of_use)
+    val sTerms = SpannableString(termsItem.title)
+    sTerms.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
+        0,
+        sTerms.length,
+        0
+    )
+    termsItem.title = sTerms
 
-        val exitItem = myMenu.findItem(R.id.nav_exit)
-        val sExit = SpannableString(exitItem.title)
-        sExit.setSpan(
-            TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
-            0,
-            sExit.length,
-            0
-        )
-        exitItem.title = sExit
+    val exitItem = myMenu.findItem(R.id.nav_exit)
+    val sExit = SpannableString(exitItem.title)
+    sExit.setSpan(
+        TextAppearanceSpan(this, R.style.NavigationSubTitleAppearance),
+        0,
+        sExit.length,
+        0
+    )
+    exitItem.title = sExit
 
-    }
+}
 
 
     override fun onSupportNavigateUp(): Boolean {
