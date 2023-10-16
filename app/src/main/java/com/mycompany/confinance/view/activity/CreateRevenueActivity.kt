fe1b68c41332302f7135d58e2c39852c065f0df9
Selton A. De Melo <selton.alvess1@gmail.com>
2023-10-16 15:39:08 -0300

    arquivo .xml da tela CreateRevenueActivity feita com sucesso.

--- app/src/main/java/com/mycompany/confinance/view/activity/CreateRevenueActivity.kt
+++ app/src/main/java/com/mycompany/confinance/view/activity/CreateRevenueActivity.kt
@@ -3,10 +3,13 @@ package com.mycompany.confinance.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mycompany.confinance.R
+import com.mycompany.confinance.databinding.ActivityCreateRevenueBinding
 
 class CreateRevenueActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateRevenueBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_revenue)
+        binding = ActivityCreateRevenueBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
